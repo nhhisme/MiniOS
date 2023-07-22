9f021939c2eb4b62b5bb42a1a088b6ed2bd95c89
nhhisme <140241832+nhhisme@users.noreply.github.com>
2023-07-22 16:35:47 +0700

    Add files via upload

---
+++ minios_termux.sh
@@ -0,0 +1,19 @@
+clear
+echo "Please wait, MiniOS will be installed on Termux! Please make sure your network connection is stable. The installation will start in 3 second(s)"
+sleep 1
+clear
+echo "Please wait, MiniOS will be installed on Termux! Please make sure your network connection is stable. The installation will start in 2 second(s)"
+sleep 1
+clear
+echo "Please wait, MiniOS will be installed on Termux! Please make sure your network connection is stable. The installation will start in 1 second(s)"
+sleep 1
+clear
+echo "Please wait, MiniOS will be installed on Termux! Please make sure your network connection is stable. The installation will start in 0 second(s)"
+pkg install x11-repo
+pkg install dosbox
+pkg install xterm
+pkg install fluxbox
+pkg install git
+git clone https://github.com/nhhisme/MiniOS/
+cd MiniOS
+./main.sh
